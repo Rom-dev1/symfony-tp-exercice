c08dfc283fd979b82addcaf9bc02a3912c0d173c
Romain Trochu <romain.trochu@gmail.com>
2023-03-15 15:54:20 +0100

    first exercice

--- src/Controller/ColorController.php
+++ src/Controller/ColorController.php
@@ -7,12 +7,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ColorController extends AbstractController
-{
-    #[Route('/color', name: 'app_color')]
+{   
+    #[Route('/color', name:'app_color')]
     public function index(): Response
     {
-        return $this->render('color/index.html.twig', [
+        return $this->render('color/index.html.twig');
+    }
+
+    #[Route('/color/{color}', name: 'app_color_one')]
+    public function displayOne($color): Response
+    {   
+        
+        return $this->render('color/one.html.twig', [
             'controller_name' => 'ColorController',
+            'color' => $color
         ]);
     }
 }
--- src/Controller/DateController.php
+++ src/Controller/DateController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DateController extends AbstractController
 {
-    #[Route('/date', name: 'app_date')]
+    #[Route('/date/now', name: 'app_date')]
     public function index(): Response
-    {
+    {   
+        date_default_timezone_set('Europe/Paris');
+        $date = date('d/m/Y H:i:s');
+
         return $this->render('date/index.html.twig', [
-            'controller_name' => 'DateController',
+            'date' => $date,
         ]);
     }
 }
